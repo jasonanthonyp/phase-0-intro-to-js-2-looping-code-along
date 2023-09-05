f749d522e4d5c97bda998661417bd89f8709fe05
Jason Phillips <jasonanthonyp@gmail.com>
2023-09-05 19:29:21 -0400

    commit tree 8da5d7bdb6ac1a23448d61533c5b894cc5f3ca71

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+
+
+
+function writeCards(names, event) {
+
+  let messages = [];
+  for (let i = 0; i < names.length; i++) {
+    const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    messages.push(message);
+  }
+  return messages
+
+}
+
+console.log(writeCards(['Jason', 'Tyler', 'Thony'], 'birthday'));
+
+
+function countDown() {
+  let i = 10;
+  while (i >= 0) {
+    console.log(i--);
+  }
+
+}
